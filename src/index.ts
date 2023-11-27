e90fd229e632f4c2d2dd53f4bc5c3f12e1ec4035
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-11-27 10:09:32 -0300

    fix index and create router

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
-import { Express } from 'express';
+import express from 'express';
+
+import Router from './router';
 
 import HealthController from './controllers/Health';
-export default class Router {
-    constructor(
-        readonly httpServer: Express,
-        private readonly healthController: HealthController
-    ) {}
 
-    async init() {
-        this.httpServer.all('/', (req, res) =>
-            this.healthController.secureHandle(req, res)
-        );
-    }
-}
+(async () => {
+    const port = process.env.PORT || 3000;
+    const httpServer = express();
+
+    httpServer.use(express.json());
+    httpServer.use(express.urlencoded({ extended: true }));
+
+    const healthController = new HealthController();
+
+    const router = new Router(httpServer, healthController);
+
+    router.init();
+
+    httpServer.listen(port, () => {
+        console.log(`Listening on port: ${port}`);
+    });
+})();
